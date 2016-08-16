0a5fa7f9e368420f76b7549885ca0190964aefff
Thao Vo <ttthao.vo@gmail.com>
2016-08-16 15:35:27 +0100

    new bike class and unit tests for bike and docking_station passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'expects DockingStation instances to respond_to the method release_bike' do
-    expect(DockingStation).to respond_to(release_bike)  
+it  { is_expected.to respond_to :release_bike  }
   end
-
-end
