1b76bb5d1e682409b57f435bd4f29a8990a745b6
supasuma <supasuma@gmail.com>
2016-08-17 15:06:03 +0100

    11 - refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
-    
+    @bike = bike
   end
 
-  def bike
 
-  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,8 @@ require 'docking_station'
 describe DockingStation do
   it  { is_expected.to respond_to :release_bike  }
 
+
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
@@ -18,5 +20,11 @@ describe DockingStation do
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
