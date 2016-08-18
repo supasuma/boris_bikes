a9cec9681fef5b9a98356f04312cafbea37e2ed1
supasuma <supasuma@gmail.com>
2016-08-18 12:48:08 +0100

    added constant

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,7 +29,8 @@ describe DockingStation do
     end
 
     it 'raises an error when docking station is full' do
-      20.times { subject.dock(Bike.new) }
+      #stub_const('DockingStation::DEFAULT_CAPACITY')
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new)}.to raise_error 'Docking station is full'
     end
   end
