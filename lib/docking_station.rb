0587f61b82413b235bf04989d67b7003e2cbf0b0
Thao Vo <ttthao.vo@gmail.com>
2016-08-18 11:56:37 +0100

    15. - adde methods empty and full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,25 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' unless @bikes.length >= 1
+    fail 'No bikes available' if self.empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if @bikes.length >= 20
+    fail "Docking station is full" if self.full?
   @bikes << bike
+  end
+
+
+  def full?
+    if @bikes.length >= 20
+      true
+    end
+  end
+
+  def empty?
+    if @bikes.empty?
+      true
+    end
   end
 end
