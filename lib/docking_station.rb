23c45c3b99b0039a3143a6064392778a082020b4
Thao Vo <ttthao.vo@gmail.com>
2016-08-18 12:16:29 +0100

    Privatise methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,15 +8,16 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if self.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if self.full?
+    fail "Docking station is full" if full?
   @bikes << bike
   end
 
+private
 
   def full?
     if @bikes.length >= 20
